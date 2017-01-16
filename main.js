6b7565e41615cee44aee1f0719f627eecd87e6d7
David Gilman <daviidgilman@gmail.com>
2017-01-16 04:14:34 -0800

    changes made

--- main.js
+++ main.js
@@ -6,11 +6,30 @@ import {
   View,
 } from 'react-native';
 import Exponent from 'exponent';
+import Clarifai from 'clarifai';
 
 export default class App extends React.Component {
   state = {
     image: null,
+    message: ''
   }
+  constructor()) {
+  // instantiate a new Clarifai app passing in your clientId and clientSecret
+  var app = new Clarifai.App(
+    'xxx',
+    'xxx'
+  );
+
+  // predict the contents of an image by passing in a url
+  app.models.predict(Clarifai.GENERAL_MODEL, 'http://cdn2.thr.com/sites/default/files/imagecache/scale_crop_768_433/2016/05/fruit_ninja.jpg').then(
+    function(response) {
+      console.log(response);
+    },
+    function(err) {
+      console.error(err);
+    }
+  );
+}
 
   render() {
     let { image } = this.state;
